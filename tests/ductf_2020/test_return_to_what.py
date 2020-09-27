cf3b84b2c2c517715b3ea5e94e1b2eb85133488c
Mariusz Skoneczko <mariusz@skoneczko.com>
2020-09-27 22:04:31 +1000

    Actually test that we get some remote code execution

--- tests/ductf_2020/test_return_to_what.py
+++ tests/ductf_2020/test_return_to_what.py
@@ -5,6 +5,9 @@ def test_return_to_what():
     BIN = "./tests/ductf_2020/return-to-what"
     s = PwnState(BIN, process(BIN))
     classic(s)
+    s.target.clean(1)
+    s.target.sendline("echo $0")
+    assert s.target.readline() == b"/bin/sh\n"
 
 
 if __name__ == "__main__":
