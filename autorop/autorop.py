d87d8d933438c2951d529602105ffcb2c0539c48
Mariusz Skoneczko <mariusz@skoneczko.com>
2020-09-30 18:41:42 +1000

    Refactor CLI to reduce code repetition

--- autorop/autorop.py
+++ autorop/autorop.py
@@ -7,13 +7,14 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         # exploit local binary
         binary = sys.argv[1]
-        result = turnkey.classic(PwnState(binary, process(binary)))
-        result.target.interactive()
+        state = PwnState(binary, process(binary))
     elif len(sys.argv) == 4:
         # exploit remote
         binary, host, ip = sys.argv[1:]
-        result = turnkey.classic(PwnState(binary, remote(host, int(ip))))
-        result.target.interactive()
+        state = PwnState(binary, remote(host, int(ip)))
     else:
         print("Usage: autorop BINARY [HOST IP]")
         exit()
+
+    result = turnkey.classic(state)
+    result.target.interactive()
