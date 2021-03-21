dc96b81e7f0ac12bdf4b35cd9e64a5d1dfce47eb
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-03-22 10:34:31 +1100

    Rename UbuntuDockerfile to DebianDockerfile to be more accurate

--- ci/build.sh
+++ ci/build.sh
@@ -8,7 +8,7 @@ docker pull $LIBC_DATABASE_IMAGE
 
 if [[ $1 == ubuntu:* || $1 == debian:* ]]
 then
-    docker build -f UbuntuDockerfile --build-arg "LIBC_DATABASE_IMAGE=$LIBC_DATABASE_IMAGE" --build-arg "UBUNTU_IMAGE=$1" -t "autorop-test-$1" ..
+    docker build -f DebianDockerfile --build-arg "LIBC_DATABASE_IMAGE=$LIBC_DATABASE_IMAGE" --build-arg "DEBIAN_IMAGE=$1" -t "autorop-test-$1" ..
 else
     echo "Unknown image '$1'"
     exit 1
