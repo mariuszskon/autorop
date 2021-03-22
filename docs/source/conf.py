e0118cdd6add5c25a7f6efaffffced0e3d6e8450
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-03-22 15:49:39 +1100

    Make navigation sidebar in documentation fully show useful parts

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,6 +59,7 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "collapse_navigation": False,
+    "navigation_depth": 5,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
