fae158d8391245b76735525a9ff4375fe6f31348
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-03-24 10:27:12 +1100

    Use docker hub image for libc

--- ci/build.sh
+++ ci/build.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-docker build -f LibcdbDockerfile -t "autorop-libcdb-builder" ..
+LIBC_DATABASE_IMAGE="mariuszskon/libc-database:2021.03.23"
+
+docker pull $LIBC_DATABASE_IMAGE
 
 if [[ $1 == ubuntu:* || $1 == debian:* ]]
 then
-    docker build -f UbuntuDockerfile --build-arg "UBUNTU_IMAGE=$1" -t "autorop-test-$1" ..
+    docker build -f UbuntuDockerfile --build-arg "LIBC_DATABASE_IMAGE=$LIBC_DATABASE_IMAGE" --build-arg "UBUNTU_IMAGE=$1" -t "autorop-test-$1" ..
 else
     echo "Unknown image '$1'"
     exit 1
